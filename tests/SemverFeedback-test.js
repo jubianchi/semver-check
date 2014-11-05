a30332eb301d086d1c360440c84f1212a9271ca1
Olivier Balais <obalais@gmail.com>
2014-11-05 15:33:41 +0000

    Added feedback component test

---
+++ tests/SemverFeedback-test.js
@@ -0,0 +1,33 @@
+jest.dontMock('../src/components/semver-feedback.jsx');
+
+describe('SemverFeedback', function() {
+    var React = require('react/addons');
+    var SemverFeedback = require('../src/components/semver-feedback.jsx');
+    var TestUtils = React.addons.TestUtils;
+
+    it('should give success feedback', function() {
+        var feedback = TestUtils.renderIntoDocument(SemverFeedback({
+            satisfies: true,
+            version: "X",
+            constraint: "Y"
+        }));
+
+        expect(feedback.getDOMNode().textContent).toContain('X satisfies constraint Y');
+    });
+
+    it('should give error feedback', function() {
+        var feedback = TestUtils.renderIntoDocument(SemverFeedback({
+            satisfies: false,
+            version: "X",
+            constraint: "Y"
+        }));
+
+        expect(feedback.getDOMNode().textContent).toContain('X does not satisfy constraint Y');
+    });
+
+    it('should give initial feedback', function() {
+        var feedback = TestUtils.renderIntoDocument(SemverFeedback());
+
+        expect(feedback.getDOMNode().textContent).toContain('Enter a constraint and a version to check if they match');
+    });
+});
