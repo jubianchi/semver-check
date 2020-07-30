75afba795b81a73c37528844622e630bb6a436e9
jubianchi <jubianchi@users.noreply.github.com>
2020-07-30 20:12:56 +0000

    deploy: 46cfe2317981367f1b569f2059e0e647a563a0ed

---
+++ precache-manifest.7cf439065b3208b5abc27cd8f50468f4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c991c63b0a1a474943b290cd61141068",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de33ab876bb15bad5bce",
+    "url": "./static/css/2.6330210c.chunk.css"
+  },
+  {
+    "revision": "ef36fc8c38685335d613",
+    "url": "./static/css/main.0f67c94c.chunk.css"
+  },
+  {
+    "revision": "de33ab876bb15bad5bce",
+    "url": "./static/js/2.76beeaec.chunk.js"
+  },
+  {
+    "revision": "7e2d658e22451d51ee4e222132e64124",
+    "url": "./static/js/2.76beeaec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef36fc8c38685335d613",
+    "url": "./static/js/main.d4538d0f.chunk.js"
+  },
+  {
+    "revision": "6d19969b40a3c0481103",
+    "url": "./static/js/runtime-main.cef4ef88.js"
+  },
+  {
+    "revision": "190e72572ee84190f4777765fa49fc47",
+    "url": "./static/media/octicons.190e7257.svg"
+  },
+  {
+    "revision": "d038ccbc4a99be24f33a54b482b2422e",
+    "url": "./static/media/octicons.d038ccbc.eot"
+  },
+  {
+    "revision": "de59a97248b44599e6747a27a943f738",
+    "url": "./static/media/octicons.de59a972.woff2"
+  },
+  {
+    "revision": "e0d4a324833e13be7d4fa762146d0a71",
+    "url": "./static/media/octicons.e0d4a324.ttf"
+  },
+  {
+    "revision": "ee5b1bee959a95bd43b223ec901d098a",
+    "url": "./static/media/octicons.ee5b1bee.woff"
+  }
+]);
